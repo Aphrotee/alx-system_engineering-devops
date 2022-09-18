2128d9e92bff1b73728a1bf3f2c29f4df6cc8449
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-09-18 01:57:37 +0000

    task 3

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+# Creates an ssh config file to bypass passphrase
+file { '~/.ssh/config':
+  ensure  => present,
+  path    => '~/.ssh/config',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'Host *   PasswordAuthentication no\n    IdentityFile ~/.ssh/school\n    SendEnv LANG LC_*\n    HashKnownHosts yes\n    GSSAPIAuthentication yes\n    GSSAPIDelegateCredentials no'
+}
