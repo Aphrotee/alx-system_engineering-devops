57963d3acd433a295cf9aad82301b8559cf9e495
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-09-18 22:27:01 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,11 @@
 # Creates an ssh config file to bypass passphrase
 #!/use/bin/env bash
-file { '~/.ssh/config':
-  ensure  => present,
-  path    => '~/.ssh/config',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => "Host *   PasswordAuthentication no\n    IdentityFile ~/.ssh/school\n    SendEnv LANG LC_*\n    HashKnownHosts yes\n    GSSAPIAuthentication yes\n    GSSAPIDelegateCredentials no"
+file_line { 'Declare_identity_file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/holberton',
+}
+
+file_line { 'Turn_off_passwd_auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
 }
